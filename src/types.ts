7b2fc3e86c2d41d907ab758f38e8c4a9be431d22
Cristopher <cristopher@gdplabs.id>
2026-01-29 08:13:17 +0700

    chore: remove SLACK_BOT_TOKEN

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,5 @@
 export interface Env {
   ENVIRONMENT_RESERVATION: KVNamespace;
-  SLACK_BOT_TOKEN: string;
 
   SERVICE_ACCOUNT_EMAIL: string;
   SERVICE_ACCOUNT_PRIVATE_KEY: string;
@@ -16,12 +15,12 @@ export interface GoogleChatEvent {
     text: string;
     slashCommand?: {
       commandId: number;
-    }
-  }
+    };
+  };
   user: {
     name: string;
     displayName: string;
     email: string;
     type: 'HUMAN' | 'BOT';
-  }
+  };
 }
