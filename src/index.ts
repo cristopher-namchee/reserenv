d9a1d32a425d676cc7c6dcd2de1576c408958086
Cristopher <cristophernamchee12@gmail.com>
2026-02-08 19:38:10 +0700

    chore: add wrangler vars

--- src/index.ts
+++ src/index.ts
@@ -38,12 +38,6 @@ app.post('/', async (c) => {
   return c.json({});
 });
 
-app.get('/send-reminder', async (c) => {
-  await sendReminder(c.env);
-
-  return c.json({ message: 'Reminder sent!' });
-});
-
 app.onError((e, c) => {
   console.error(e);
 
